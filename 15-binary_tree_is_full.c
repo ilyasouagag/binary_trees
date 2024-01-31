16826a234354cb3602e0b4f9473efedb85b8662d
Alaa Eddine <Alaaelhannaoui@proton.me>
2024-01-31 19:34:27 +0100

    15 done

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,7 +1,6 @@
 #include "binary_trees.h"
 
-int binary_tree_leaf_checker(binary_tree_t *tree);
-int binary_tree_is_full(const binary_tree_t *tree);
+int is_full_recursive(const binary_tree_t *tree);
 /**
  * binary_tree_is_full - check if the binary tree is full
  * @tree: root node of the binary tree
@@ -9,26 +8,27 @@ int binary_tree_is_full(const binary_tree_t *tree);
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (!tree)
+	if (tree == NULL)
 		return (0);
-
-	return (binary_tree_leaf_checker(tree));
+	return (is_full_recursive(tree));
 }
 
 /**
- * binary_tree_leaf_checker - check if a node is a leaf
+ * is_full_recursive - check if a node is a leaf
  * @tree: root node of the binary tree
  * Return: 0 or 1 it is a leaf
  */
-int binary_tree_leaf_checker(binary_tree_t *tree)
+int is_full_recursive(const binary_tree_t *tree)
 {
-	if (tree)
+	if (tree != NULL)
 	{
 		if ((tree->left != NULL && tree->right == NULL) ||
 			(tree->left == NULL && tree->right != NULL) ||
-			binary_tree_leaf_checker(tree->left) == 0 ||
-			binary_tree_leaf_checker(tree->right) == 0)
+			is_full_recursive(tree->left) == 0 ||
+			is_full_recursive(tree->right) == 0)
+
 			return (0);
 	}
+
 	return (1);
 }
